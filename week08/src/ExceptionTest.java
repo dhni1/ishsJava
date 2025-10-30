1237eb14c925412a81d60b1ec4e3f006f077ffb6
dhni1 <dohoon413@gmail.com>
2025-10-30 20:28:55 +0900

    exception v0.2

--- week08/src/ExceptionTest.java
+++ week08/src/ExceptionTest.java
@@ -4,9 +4,15 @@ public class ExceptionTest {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         System.out.print("Input numerator : ");
-        double numerator = scanner.nextInt();
+        int numerator = scanner.nextInt();
         System.out.print("Input denominator : ");
-        double denominator = scanner.nextInt();
+        int denominator = scanner.nextInt();
+
+        if(denominator == 0){
+            System.out.println("분모가 0이 될 수 없습니다.");
+        }{
+            System.out.println(numerator/denominator);
+        }
 
         System.out.println(numerator/denominator);
     }
