f68acc7268d0bf478e3b0d09462a802b7ab595e3
dhni1 <dohoon413@gmail.com>
2025-11-06 19:42:58 +0900

    collection v0.2

--- week09/src/collections/CollectionDemo.java
+++ week09/src/collections/CollectionDemo.java
@@ -1,16 +1,30 @@
 package collections;
 
+import java.util.ArrayList;
+
 public class CollectionDemo {
     public static void main(String[] args) {
-        Fruit[] fruits = {
-                new Fruit("Kiwi", 3000),
-                new Fruit("Strawberry", 2500),
-        };
+        ArrayList<Fruit> fruits = new ArrayList<>();
+//        System.out.println(fruits.isEmpty());
+
+        fruits.add(new Fruit("Kiwi", 3000));
+        fruits.add(new Fruit("Strawberry", 2500));
+
+        for(int i=0;i<fruits.size();i++){
+            System.out.println(fruits.get(i).getName() + " (단가:" + fruits.get(i).getPrice() + ")");
+        }
+
+//        Fruit[] fruits = {
+//                new Fruit("Kiwi", 3000),
+//                new Fruit("Strawberry", 2500),
+//        };
 //
 //        System.out.println(fruit.getName() + " (단가:" + fruit.getPrice() + ")");
 
-        for(int i=0; i<fruits.length; i++){
-            System.out.println(fruits[i].getName() + " (단가:" +  fruits[i].getPrice() + ")");
-        }
+//        for(int i=0; i<fruits.length; i++){
+//            System.out.println(fruits[i].getName() + " (단가:" +  fruits[i].getPrice() + ")");
+//        }
+//
+//
     }
 }
