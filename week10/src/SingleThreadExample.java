1b80a02cb6141a031ad686ba15afac803e273978
dhni1 <dohoon413@gmail.com>
2025-11-19 19:47:09 +0900

    thread v0.1

---
+++ week10/src/SingleThreadExample.java
@@ -0,0 +1,24 @@
+public class SingleThreadExample {
+    private static void runTask(String name){
+        for(int i = 1; i<=5; i++){
+            System.out.println(name + "thread" + i);
+            try {
+                Thread.sleep(500); // 0.5초 대기
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+        }
+    }
+
+
+    public static void main(String[] args) {
+        long startTime =  System.currentTimeMillis(); //record project starting time
+        runTask("first");
+        runTask("second");
+
+        long endTime = System.currentTimeMillis();
+        long duration = endTime - startTime;
+        System.out.println("total time: " + duration + "ms");
+
+    }
+}
